626ab57790faad6c1a9cb994d4e060e47b5c6b47
Bethany Jenkins <bethanykjenkins@rocketmail.com>
2019-09-04 03:42:37 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,5 +1,4 @@
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   phrase = "It's-a me, Mario!"
   puts phrase
 end
@@ -7,5 +6,8 @@ end
 p mario
 
 def toadstool
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
+p toadstool
